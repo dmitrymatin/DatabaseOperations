beaa058b1306334e86e1f7c1d3b9a49ef7c02b86
Dmitry Matin <super.dmitry320@yandex.ru>
2020-12-08 19:08:21 +0300

    simplify while loop by using 'NOT IN'

--- part_2/backup_all_dbs.sql
+++ part_2/backup_all_dbs.sql
@@ -1,11 +1,7 @@
---select * from sys.databases
---DECLARE @d DATE = GETDATE();
---SELECT FORMAT(@d, '-yyyy-MM-dd') AS 'DATAA'
-
---SELECT GETDATE();
-
 DECLARE completeBackupCursor CURSOR
-	FOR SELECT [name] FROM sys.databases;
+	FOR 
+	SELECT [name] FROM sys.databases
+		WHERE [name] NOT IN ('master', 'model', 'msdb', 'tempdb');
 
 DECLARE @path NVARCHAR(max);
 DECLARE @filename NVARCHAR(max);
@@ -15,15 +11,11 @@ OPEN completeBackupCursor;
 FETCH NEXT FROM completeBackupCursor INTO @db_name;
 WHILE @@FETCH_STATUS = 0
 	BEGIN
-		IF @db_name <> 'master' AND @db_name <> 'model' AND @db_name <> 'msdb' AND @db_name <> 'tempdb'
-		BEGIN
-			SET @filename = @db_name + FORMAT(GETDATE(), '-yyyy-MM-dd');
-			SET @path = 'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\Backup\' + @filename + '.bak';
-			BACKUP DATABASE @db_name TO DISK = @path
-			PRINT 'backed up database ' + @db_name + ' to ' + @path;
-		END
-		ELSE
-			PRINT 'BYE';
+		SET @filename = @db_name + FORMAT(GETDATE(), '-yyyy-MM-dd');
+		SET @path = 'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\Backup\' + @filename + '.bak';
+		BACKUP DATABASE @db_name TO DISK = @path
+		PRINT 'backed up database ' + @db_name + ' to ' + @path;
+
 		FETCH NEXT FROM completeBackupCursor INTO @db_name;
 	END;
 
